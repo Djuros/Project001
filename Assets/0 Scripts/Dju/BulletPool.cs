9c1067126fbfad543d4ca2552df347cfd0797710
INEOR\đ.bulajic <djuro.bulajic@ineor.si>
2022-07-26 15:14:47 +0200

    Bullet System

--- Assets/0 Scripts/Dju/BulletPool.cs
+++ Assets/0 Scripts/Dju/BulletPool.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
+using Photon.Realtime;
 public class BulletPool : MonoBehaviour
 {
-    
+    public static BulletPool ins;
+    public Bullet bullet_prefab;
+    public List<Bullet> bullets = new List<Bullet>();
+    public Transform muzzle;
+    public int initial_pool_size;
+    private void Awake()
+    {
+        ins = this;
+    }
+    void Spawn_Bullets() {
+        for (int i =0; i < initial_pool_size; i++) {
+            GameObject _bullet = PhotonNetwork.Instantiate("Bullet",Vector3.zero, Quaternion.identity);
+            bullets.Add(_bullet.GetComponent<Bullet>());
+            _bullet.gameObject.SetActive(false);
+        }
+    }
+    public void Fire()
+    {
+        Bullet _bullet = null;
+        for (int i = 0; i < bullets.Count; i++) {
+            if (!bullets[i].gameObject.activeInHierarchy) { _bullet = bullets[i]; break; }
+        }
+        if (_bullet == null) {
+            _bullet = Instantiate(bullet_prefab);
+            bullets.Add(_bullet);
+        }
+        _bullet.Eject(muzzle.position, muzzle.forward);
+    }
 }
