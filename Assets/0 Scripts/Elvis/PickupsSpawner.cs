c6b4a7f25688f93f170e935279907c7fc7c61f1d
ElvisMujic <elviznation@gmail.com>
2022-07-28 08:53:23 +0200

    Pickups added (Health, Speed, Extra damage, Increased shooting range)

--- Assets/0 Scripts/Elvis/PickupsSpawner.cs
+++ Assets/0 Scripts/Elvis/PickupsSpawner.cs
@@ -2,47 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlagSpawner : MonoBehaviour
+public class PickupsSpawner : MonoBehaviour
 {
     public List<Transform> spawnPoints, randomized_pool = new List<Transform>();
-    public List<GameObject> flags = new List<GameObject>();
-    public GameObject flag_prefab;
+    public List<GameObject> pickups = new List<GameObject>();
+    public GameObject[] pickup_prefab;
     int randomSpawnPoint;
-    public int flagLimit = 1;
+    public int pickupsLimit = 4;
     public static bool SpawnEnabled;
 
     private void Start()
     {
-        SpawnFlags();
+        SpawnPickups();
     }
     void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            ReSpawnFlags();
+            RespawnPickups();
         }
     }
-   
-    void SpawnFlags()
+
+    void SpawnPickups()
     {
         randomized_pool = new List<Transform>(spawnPoints);
-        for (int i = 0; i < flagLimit; i++) 
+        for (int i = 0; i < pickupsLimit; i++)
         {
-            GameObject _go = Instantiate(flag_prefab, Vector3.zero, Quaternion.identity);
-            flags.Add(_go);
+            GameObject _go = Instantiate(pickup_prefab[Random.Range(0, pickup_prefab.Length - 1)], Vector3.zero, Quaternion.identity);
+            pickups.Add(_go);
             Transform tmp_sp = randomized_pool[Random.Range(0, randomized_pool.Count)];
             _go.transform.position = tmp_sp.position;
             randomized_pool.Remove(tmp_sp);
         }
     }
-    public void ReSpawnFlags()
+    public void RespawnPickups()
     {
         randomized_pool = new List<Transform>(spawnPoints);
-        for (int i = 0; i < flags.Count; i++)
+        for (int i = 0; i < pickups.Count; i++)
         {
-            flags[i].SetActive(true);
+            pickups[i].SetActive(true);
             Transform tmp_sp = randomized_pool[Random.Range(0, randomized_pool.Count)];
-            flags[i].transform.position = tmp_sp.position;
+            pickups[i].transform.position = tmp_sp.position;
             randomized_pool.Remove(tmp_sp);
         }
     }
