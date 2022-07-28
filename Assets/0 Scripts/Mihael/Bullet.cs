8ded57fdab36a7214cb84c2e35c953b082049572
MihaelGolob <golob.mihael@gmail.com>
2022-07-28 09:09:32 +0200

    created a impact particle effect that is played on bullet impact

--- Assets/0 Scripts/Mihael/Bullet.cs
+++ Assets/0 Scripts/Mihael/Bullet.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Resources;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour { 
     // inspector assigned
     [SerializeField] private int _speed = 5;
     [SerializeField] private int _timeToLive = 10;
+    [SerializeField] private ParticleSystem _impactEffect;
     
     // internal variables
     private Rigidbody _rb;
@@ -22,7 +24,12 @@ public class Bullet : MonoBehaviour {
     }
     
     private void OnCollisionEnter(Collision collision) {
-        // destroy bullet on collision
-        Destroy(gameObject);
+        // disable components
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+        // stop bullet
+        _rb.velocity = Vector3.zero;
+        // play particle effect
+        _impactEffect.Play();
     }
 }
