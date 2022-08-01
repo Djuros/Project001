05f6d840f6229fd2dd5bdbd9c61c7577f0def860
Adrian Lorenčič <adrianlorencic98@gmail.com>
2022-08-01 08:59:09 +0200

    Lobby shows who else is in the room

--- Assets/0 Scripts/Adrian/LobbyManager.cs
+++ Assets/0 Scripts/Adrian/LobbyManager.cs
@@ -5,18 +5,22 @@ using Photon.Realtime;
 using Photon.Pun;
 using UnityEngine.UI;
 using System;
+using TMPro;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
     public InputField roomInputField, joinRoomInput;
     public GameObject lobbyPanel, roomPanel, startButton;
+    public GameObject[] playersInRoomTextsGO;
     public Text roomName;
+    public TextMeshProUGUI aloneInRoomText;
 
     public RoomItem roomItemPrefab;
     List<RoomItem> roomItemsList = new List<RoomItem>();
     public Transform contenctObject;
     public float timeBetweenUpdates = 1.5f;
     float nextUpdateTime;
+    private string masterClientName;
 
     private void Start()
     {
@@ -25,13 +29,39 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     private void Update()
     {
-        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount >= 2) 
+        if (!PhotonNetwork.InRoom) {return;}
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
         {
-            startButton.SetActive(true);
+            if (PhotonNetwork.PlayerList[i].IsMasterClient)
+            {
+                masterClientName = PhotonNetwork.PlayerList[i].NickName;
+            }
+        }
+        if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount >= 2)
+        {
+            startButton.GetComponent<Button>().interactable = true;
+            aloneInRoomText.text = "";
+        }
+        else if (!PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount >= 2)
+        {
+            startButton.GetComponent<Button>().interactable = false;
+            aloneInRoomText.text = "Only player " + masterClientName +  " can start the game!";
         }
         else
         {
-            startButton.SetActive(false);
+            startButton.GetComponent<Button>().interactable = false;
+            aloneInRoomText.text = "You are alone in the room. Wait for someone to join!";
+        }
+
+        for (int i = 0; i < playersInRoomTextsGO.Length; i++)
+        {
+            playersInRoomTextsGO[i].SetActive(false);
+        }
+        
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            playersInRoomTextsGO[i].SetActive(true);
+            playersInRoomTextsGO[i].GetComponent<Text>().text = PhotonNetwork.PlayerList[i].NickName;
         }
     }
 
@@ -106,5 +136,10 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         PhotonNetwork.JoinLobby();
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
     }
 }
